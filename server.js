469c78e65ba0ef4efbfe11397eb3b655fdc32a9a
thilina15 <thibbatz@gmail.com>
2021-03-23 02:52:48 +0530

    static file setup

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express()
 app.use('/',indexRoute)
 app.set('view engine', 'ejs')
 app.set('views',__dirname+'/views')
+app.use('/assets',express.static('public'))
+app.use('/public',express.static('public'))
+
+app.use('/js',express.static('public/js'))
 
 
 
